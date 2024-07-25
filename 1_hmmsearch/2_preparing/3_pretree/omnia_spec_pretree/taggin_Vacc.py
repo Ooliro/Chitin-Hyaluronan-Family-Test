91c4e1cc0b36e3db6fb03ef84d1dbf021031512a
Raul Rosas <raulolivaresrosas@gmail.com>
2024-07-25 01:09:29 -0600

    Actualización 3

---
+++ 1_hmmsearch/2_preparing/3_pretree/omnia_spec_pretree/taggin_Vacc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+
+#input_file = sys.argv[1]
+#output_file = 'modified_acc_taggd.fasta'
+
+def process_fasta(input_file, output_file):
+    with open(input_file, 'r') as f_in, open(output_file, 'w') as f_out:
+        for line in f_in:
+            if line.startswith('>'):  # Check if the line is a header line
+                # Split the header line by whitespace and keep the first part (accession number)
+                accession_number = line.split('.')[0][1:]
+                # Write the modified header to the output file
+                f_out.write(f'>{accession_number}\n')
+            else:
+                # Write sequence lines as they are to the output file
+                f_out.write(line)
+
+input_file = sys.argv[1]
+ouput_file = 'modified_acc_taggd.fasta'
